62daa969b8aa64f47baa5c94fc87f4e9ef89a950
Matis <matisleima@gmail.com>
2023-09-06 09:57:33 +0300

    Esimene commit2

--- Database/import.sql
+++ Database/import.sql
@@ -2,4 +2,13 @@ INSERT INTO "user" (id, first_name, last_name, password, status, role_id, image_
 VALUES (DEFAULT, 'Matis', 'Leima', '123', 'A', DEFAULT, DEFAULT, DEFAULT, 'matisleima@gmail.com')
 
 INSERT INTO district (id, name)
-VALUES (DEFAULT, 'Vanalinn')
+VALUES (DEFAULT, 'Vanalinn'),
+       (DEFAULT, 'Kesklinn'),
+       (DEFAULT, 'Mustamäe'),
+       (DEFAULT, 'Lasnamäe'),
+       (DEFAULT, 'Pirita'),
+       (DEFAULT, 'Kristiine'),
+       (DEFAULT, 'Nõmme'),
+       (DEFAULT, 'Õismäe'),
+       (DEFAULT, 'Põhja-Tallinn');
+
