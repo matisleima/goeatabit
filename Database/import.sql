4084f4cd815b2e9f952aaadbc3e908d31ef590b3
Matis <matisleima@gmail.com>
2023-09-06 11:39:58 +0300

    Andmebaasi mudel kontrollitud

--- Database/import.sql
+++ Database/import.sql
@@ -13,19 +13,3 @@ INSERT INTO role (id, name)
 VALUES (DEFAULT, 'admin'),
        (DEFAULT, 'customer');
 
-INSERT INTO rating (id, level)
-VALUES (DEFAULT, 1),
-       (DEFAULT, 2),
-       (DEFAULT, 3),
-       (DEFAULT, 4),
-       (DEFAULT, 5);
-
-INSERT INTO location (id, address, district_id)
-VALUES (DEFAULT, 'Aia 6', 1),
-       (DEFAULT, 'Pargi 7', 2);
-
-INSERT INTO "user" (id, first_name, last_name, password, status, role_id, image_id, location_id, e_mail)
-VALUES (DEFAULT, 'Matis', 'Leima', '123', 'A', 1, null, 1, 'matisleima@gmail.com');
-
-INSERT INTO offer (id, time, date, price, seats, name, description, food_group_id, status, user_id, rating_id)
-VALUES (DEFAULT, )
