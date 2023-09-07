4b4ff30ace66bc6464f48fc391b50e24122cc6b6
Matis <matisleima@gmail.com>
2023-09-07 15:33:44 +0300

    Login Service complete without sad path, edited database (emails)

--- Database/import.sql
+++ Database/import.sql
@@ -16,9 +16,9 @@ INSERT INTO public.district (id, name) VALUES (7, 'Nõmme');
 INSERT INTO public.district (id, name) VALUES (8, 'Õismäe');
 INSERT INTO public.district (id, name) VALUES (9, 'Põhja-Tallinn');
 
-INSERT INTO public."user" (id, role_id, rating, status, email, password) VALUES (default, 2, 1.0, 'A', 'email1', '123');
-INSERT INTO public."user" (id, role_id, rating, status, email, password) VALUES (default, 2, 1.0, 'A', 'email2', '123');
-INSERT INTO public."user" (id, role_id, rating, status, email, password) VALUES (default, 2, 1.0, 'A', 'email3', '123');
+INSERT INTO public."user" (id, role_id, rating, status, email, password) VALUES (default, 2, 1.0, 'A', '1', '123');
+INSERT INTO public."user" (id, role_id, rating, status, email, password) VALUES (default, 2, 1.0, 'A', '2', '123');
+INSERT INTO public."user" (id, role_id, rating, status, email, password) VALUES (default, 2, 1.0, 'A', '3', '123');
 
 INSERT INTO public.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 1, 1, 'Peeter', 'Tamm');
 INSERT INTO public.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 2, 2, 'Tarmo', 'Kask');
