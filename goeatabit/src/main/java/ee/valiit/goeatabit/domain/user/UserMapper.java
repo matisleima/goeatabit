2be9f800fe8b262fc85c12e66718d9478ca244a2
Matis <matisleima@gmail.com>
2023-09-11 10:36:38 +0300

    POST service WIP

--- goeatabit/src/main/java/ee/valiit/goeatabit/domain/user/UserMapper.java
+++ goeatabit/src/main/java/ee/valiit/goeatabit/domain/user/UserMapper.java
@@ -2,10 +2,6 @@ package ee.valiit.goeatabit.domain.user;
 
 import ee.valiit.goeatabit.business.login.dto.LoginResponse;
 import ee.valiit.goeatabit.business.signup.dto.SignUpRequest;
-import ee.valiit.goeatabit.domain.contact.Contact;
-import ee.valiit.goeatabit.domain.image.Image;
-import ee.valiit.goeatabit.domain.location.Location;
-import ee.valiit.goeatabit.domain.rating.Rating;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
@@ -18,16 +14,4 @@ public interface UserMapper {
     @Mapping(source = "password", target = "password")
     @Mapping(constant = "0.0", target = "rating")
     User toUser(SignUpRequest signUpRequest);
-
-    @Mapping(source = "imageData", target = "data")
-    Image toImage(SignUpRequest signUpRequest);
-
-    @Mapping(source = "firstName", target = "firstname")
-    @Mapping(source = "lastName", target = "lastname")
-    Contact toContact(SignUpRequest signUpRequest);
-
-    @Mapping(source = "districtId", target = "district.id")
-    @Mapping(source = "address", target = "address")
-    Location toLocation(SignUpRequest signUpRequest);
-
 }
