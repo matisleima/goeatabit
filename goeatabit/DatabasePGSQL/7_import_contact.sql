bbc6a0d0ffef2770461f2eb0dbd4a2319e2464bf
Matis Leima <matisleima@gmail.com>
2023-11-01 11:29:23 +0200

    User image and gender correlation fixed 2

--- goeatabit/DatabasePGSQL/7_import_contact.sql
+++ goeatabit/DatabasePGSQL/7_import_contact.sql
@@ -1,5 +1,5 @@
 INSERT INTO eat.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 1, 1, 'Anna', 'Tamm');
-INSERT INTO eat.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 3, 3, 'Kristi', 'Tamm');
+INSERT INTO eat.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 3, 2, 'Kristi', 'Tamm');
 INSERT INTO eat.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 4, 4, 'Ants', 'Tamm');
 INSERT INTO eat.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 5, 7, 'Jüri', 'Tamm');
 INSERT INTO eat.contact (id, user_id, image_id, firstname, lastname) VALUES (default, 6, 5, 'Tiina', 'Tamm');
